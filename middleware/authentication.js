43946c15ca339ca56000009662b36beee28af2e7
Mohsin <mohsincse2015@gmail.com>
2023-07-24 14:21:52 +0600

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+const User = require("../models/User");
+
+const auth = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach the user to the job routes
+
+    // req.user = { userId: payload.userId, name: payload.name };
+
+    req.user = {
+      userId: payload.userId,
+      password: payload.password,
+      email: payload.email,
+    };
+
+    // or
+    // req.user = await User.findById(payload.userId).select("-password");
+    // console.log("first----------", req.user);
+
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+};
+
+module.exports = auth;
